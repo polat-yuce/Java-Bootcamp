2c1a75d201dc4cdf1e1fc616fc2172c6ddc087e5
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2025-03-27 01:32:24 +0300

    Gun24 tipVeNesne

---
+++ src/Gun24/_01_Ornek/_01_TipVeNesne.java
@@ -0,0 +1,37 @@
+package Gun24._01_Ornek;
+
+//class,tip
+
+import java.util.Scanner;
+
+public class _01_TipVeNesne {
+
+    //metod
+    public static void main(String[] args) {
+        // ana metod, start burası
+        int sayi=5;
+        Scanner oku=new Scanner(System.in);
+
+        Araba sabaninArabasi=new Araba(); // Araba tipinde ismi sabaninArabasi olan bir NESNE oluşturuldu
+        sabaninArabasi.marka="Mercedes";
+        sabaninArabasi.renk="Kırmızı";
+        sabaninArabasi.motorHacmi=3000;
+        sabaninArabasi.yili=2025;
+
+        System.out.println("sabaninArabasi.yili = " + sabaninArabasi.yili);
+        System.out.println("sabaninArabasi.renk = " + sabaninArabasi.renk);
+        System.out.println("sabaninArabasi.marka = " + sabaninArabasi.marka);
+        System.out.println("sabaninArabasi.motorHacmi = " + sabaninArabasi.motorHacmi);
+
+    }
+    //metod
+}
+
+//class, tip lerin tanımlandığı yer
+
+class Araba{
+    String renk;
+    int yili;
+    String marka;
+    double motorHacmi;
+}
