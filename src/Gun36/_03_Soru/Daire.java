e1bf1edb65ee67f03625b412e580748134a29235
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2025-04-05 09:10:32 +0300

    Gun36

---
+++ src/Gun36/_03_Soru/Daire.java
@@ -0,0 +1,30 @@
+package Gun36._03_Soru;
+
+public class Daire extends Sekil{
+    private int yaricap;
+
+    public Daire(String name, int yaricap) {
+        super(name);
+        setYaricap(yaricap);
+    }
+
+    @Override
+    void alan() {
+        System.out.println("Alan="+
+                (Math.PI*this.yaricap*this.yaricap) );
+    }
+
+    @Override
+    void cevre() {
+        System.out.println("Cevre="+
+                (2*Math.PI*yaricap));
+    }
+
+    public int getYaricap() {
+        return yaricap;
+    }
+
+    public void setYaricap(int yaricap) {
+        this.yaricap = yaricap;
+    }
+}
---
+++ src/Gun36/_03_Soru/GeoMain.java
@@ -0,0 +1,16 @@
+package Gun36._03_Soru;
+
+public class GeoMain {
+    public static void main(String[] args) {
+        Dikdortgen d=new Dikdortgen("Dikdörtgen",5,6);
+        d.ciz();
+        d.alan();
+        d.cevre();
+
+        Daire dr=new Daire("Daire",6);
+        dr.ciz();
+        dr.alan();
+        dr.cevre();
+
+    }
+}
