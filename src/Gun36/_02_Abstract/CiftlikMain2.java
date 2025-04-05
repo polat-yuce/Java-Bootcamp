807d2d81114a0b8b307198286223063d23ca5143
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2025-04-05 09:05:53 +0300

    Gun36

---
+++ src/Gun36/_02_Abstract/CiftlikMain2.java
@@ -0,0 +1,11 @@
+package Gun36._02_Abstract;
+
+public class CiftlikMain2 {
+    public static void main(String[] args) {
+        Kopek2 k1=new Kopek2("Kangal",10);
+        k1.ses();
+        k1.yemek();
+        System.out.println("k1 = " + k1);
+
+    }
+}
---
+++ src/Gun36/_02_Abstract/Hayvan2.java
@@ -0,0 +1,32 @@
+package Gun36._02_Abstract;
+
+//miras+şablon 2 si birada
+//abstract = sonradan içi doldurulacak metod demek
+public abstract class Hayvan2 {
+    private String cinsi;
+    private int yas;
+
+    public Hayvan2(String cinsi, int yas) {
+        setCinsi(cinsi);
+        setYas(yas);
+    }
+
+    abstract void ses();
+    abstract void yemek();
+
+    public String getCinsi() {
+        return cinsi;
+    }
+
+    public void setCinsi(String cinsi) {
+        this.cinsi = cinsi;
+    }
+
+    public int getYas() {
+        return yas;
+    }
+
+    public void setYas(int yas) {
+        this.yas = yas;
+    }
+}
